02cc6d161ba1f45eae54ff9e13fc328dc57a2605
xthecoolboy <67747753+xthecoolboy@users.noreply.github.com>
2020-07-14 16:03:25 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,183 +1,154 @@
-const Discord = require("discord.js");
+//This Script DevelopeD By HiDDN
 
-const config = require("./config.json");
-
-// declare the client
+const Discord = require('discord.js')
 const client = new Discord.Client();
 
-client.on("message", async message => {
-  if(message.channel.type === "dm"){
-    if(message.author.bot) return;
-    if(message.content.includes("@everyone") || message.content.includes("@here")) return message.author.send("You can't use everyone/here mentions.")
-    var table = new db.table("Tickets");
-    let active = await table.get(`support_${message.author.id}`)
-    let guild = client.guilds.cache.get(config.guild);
-    let channel, found = true;
-    let user = await table.get(`isBlocked${message.author.id}`);
-    if(user === true || user === "true") return message.react("❌");
-    try {
-      if(active) client.channels.cache.get(active.channelID).guild;
-    } catch (e) {
-      found = false;
-    }
-    if(!active || !found){
-      active = {};
-      let modrole = guild.roles.cache.get(config.roles.mod);
-      let everyone = guild.roles.cache.get(guild.defaultRole.id); // not sure if this works
-      let bot = guild.roles.cache.get(config.roles.bot);
-      await table.add("ticket", 1)
-      let actualticket = await table.get("ticket");
-      channel = await guild.channels.create(`${message.author.username}-${message.author.discriminator}`, { type: 'text', reason: `Modmail created ticket #${actualticket}.` });
-      channel.setParent("571299988471152690");
-      channel.setTopic(`#${actualticket} (Open) | ${config.prefix}complete to close this ticket | Modmail for ${message.author.username} - ${message.author.id}`)
-      channel.createOverwrite(modrole, {
-        VIEW_CHANNEL: true,
-        SEND_MESSAGES: true,
-        READ_MESSAGE_HISTORY: true
-      });
-      channel.createOverwrite(everyone, {
-        VIEW_CHANNEL: false
-      });
-      channel.createOverwrite(bot, {
-        VIEW_CHANNEL: true,
-        SEND_MESSAGES: true,
-        READ_MESSAGE_HISTORY: true,
-        MANAGE_MESSAGES: true
-      })
-      let author = message.author;
-      const newChannel = new Discord.MessageEmbed()
-        .setColor(colors.success).setAuthor(author.tag, author.avatarURL())
-        .setDescription(`Ticket #${actualticket} created.\nUser: ${author}\nID: ${author.id}`)
-        .setFooter(`${moment(new Date()).format("lll")}`)
-      await client.channels.cache.get(channel.id).send(newChannel);
-      var toSendAsNew = `Hello ${author.username}, your ticket #${actualticket} has been created.`;
-      const newTicket = toSendAsNew;
-	  message.author.send(newTicket)
-      active.channelID = channel.id;
-      active.targetID = author.id;
-    }
-    channel = client.channels.cache.get(active.channelID);
-    await message.author.send()
-    var msg = message.content;
-    //var whatWeWant = msg.replace("@everyone", "[everyone]").replace("@here", `[here]`) // idk if that's useful since we're blocking mentions
-    if(message.attachments.size > 0){
-      let attachment = new Discord.MessageAttachment(message.attachments.first().url)
-      channel.send(`${message.author.username} > ${whatWeWant}`, {files: [message.attachments.first().url]})
-    } else {
-      channel.send(`${message.author.username} > ${whatWeWant}`);
-    }
-    table.set(`support_${message.author.id}`, active);
-    table.set(`supportChannel_${channel.id}`, message.author.id);
-    return;
-  }
-  if(message.author.bot) return;
-  var table = new db.table("Tickets");
-  let support = await table.get(`supportChannel_${message.channel.id}`);
-  if(support){
-    support = await table.get(`support_${support}`);
-    var user = new db.table(`Language`);
-    let lang = await user.get(`U${support.targetID}`);
-    let supportUser = client.users.cache.get(support.targetID);
-    if(!supportUser) return message.channel.delete();
-    
-    // reply (with user and role)
-    if(message.content.startsWith(`${config.prefix}reply`)){
-      var isPause = await table.get(`suspended${support.targetID}`);
-      let isBlock = await table.get(`isBlocked${support.targetID}`);
-      if(isPause === true) return message.channel.send("Ticket already paused.")
-      if(isBlock === true) return message.channel.send("User blocked.")
-      var args = message.content.split(" ").slice(1)
-      let msg = args.join(" ");
-      message.react("✅");
-      if(message.attachments.size > 0){
-        let attachment = new Discord.MessageAttachment(message.attachments.first().url)
-        return supportUser.send(`${message.author.username} > ${msg}`, {files: [message.attachments.first().url]})
-      } else {
-        return supportUser.send(`${message.author.username} > ${msg}`);
-      }
-    };
-    
-    // anonymous reply
-    if(message.content.startsWith(`${config.prefix}areply`)){
-      var isPause = await table.get(`suspended${support.targetID}`);
-      let isBlock = await table.get(`isBlocked${support.targetID}`);
-      if(isPause === true) return message.channel.send("Ticket already paused.")
-      if(isBlock === true) return message.channel.send("User blocked.")
-      var args = message.content.split(" ").slice(1)
-      let msg = args.join(" ");
-      message.react("✅");
-      return supportUser.send(`Support Team > ${msg}`);
-    };
-    
-    // print user ID
-    if(message.content === `${config.prefix}id`){
-      return message.channel.send(`User ID is **${support.targetID}**.`);
-    };
-    
-    // suspend a thread
-    if(message.content === `${config.prefix}pause`){
-      var isPause = await table.get(`suspended${support.targetID}`);
-      if(isPause === true || isPause === "true") return message.channel.send("Ticket already paused.")
-      await table.set(`suspended${support.targetID}`, true);
-      var suspend = new Discord.MessageEmbed()
-      .setDescription("⏸️ This thread has been **locked** and **suspended**. Do `b!continue` to cancel.")
-      .setTimestamp()
-      .setColor(colors.yellow)
-      message.channel.send({embed: suspend});
-	  return supportUser.send("Your ticket has been paused. We'll send you a message when we're ready to continue.")
-    };
-    
-    // continue a thread
-    if(message.content === `${config.prefix}continue`){
-      var isPause = await table.get(`suspended${support.targetID}`);
-      if(isPause === null || isPause === false) return message.channel.send("Ticket wasn't paused.");
-      await table.delete(`suspended${support.targetID}`);
-      var c = new Discord.MessageEmbed()
-      .setDescription("▶️ This thread has been **unlocked**.")
-      .setColor(colors.success).setTimestamp()
-      message.channel.send({embed: c});
-      return supportUser.send("Hi! Your ticket isn't paused anymore and we're ready to continue.");
-    }
-    
-    // block a user
-    if(message.content.startsWith(`${config.prefix}block`)){
-      var args = message.content.split(" ").slice(1)
-      let isBlock = await table.get(`isBlocked${support.targetID}`);
-      if(isBlock === true) return message.channel.send("User is already blocked")
-      let reason = args.join(" ");
-	    if(!reason || reason === "" || reason === " " || reason === undefined) reason = "Unspecified."
-      await table.set(`isBlocked${support.targetID}`, true);
-      return message.channel.send(`<:tick1:588568490496098307> This user has been blocked from using the modmail. Close the ticket with \`${config.prefix}complete\`.\nReason: ${reason}`)
-    }
-    
-    // unblock a user
-    if(message.content.startsWith(`${config.prefix}unblock`)){
-      let isBlock = await table.get(`isBlocked${support.targetID}`);
-      if(isBlock === false || !isBlock || isBlock === null) return message.channel.send("User wasn't blocked")
-      var args = message.content.split(" ").slice(1)
-      let reason = args.join(" ");
-      if(!reason || reason === "" || reason === " " || reason === undefined) reason = "Unspecified."
-      await table.delete(`isBlocked${support.targetID}`);
-      return message.channel.send(`<:tick1:588568490496098307> This user has been unblocked from using the modmail.`)
-    }
-    
-    // complete
-    if(message.content.toLowerCase() === `${config.prefix}complete`){
-        var embed = new Discord.MessageEmbed()
-        .setDescription(`Deleting this thread in **10** seconds...\n:lock: This thread has been locked and closed.`)
-        .setColor(colors.red).setTimestamp()
-        message.channel.send({embed: embed})
-        var timeout = 10000
-        setTimeout(() => {end();}, timeout)
-      }
-      async function end(){
-        table.delete(`support_${support.targetID}`)
-        let actualticket = await table.get("ticket");
-        message.channel.delete()
-        return supportUser.send(`Hello again! Your ticket #${actualticket} has been labelled as complete. Thanks for contacting us. If you wish to open a new ticket, feel free to message me.`)
-      }
-    };
-  }
-})
+function clean(text) {
+    if (typeof(text) === "string")
+      return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+    else
+        return text;
+}
 
-client.login(config.token);
+const prefix = "Prefix Here";
+const token = "Token Here";
+const embedColor = 0xE52B50;
+
+client.on("ready", () => {
+  client.user.setGame(prefix + `new`);
+});
+
+client.on("message", (message) => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  if (message.content.toLowerCase().startsWith(prefix + `help`)) {
+    const embed = new Discord.RichEmbed()
+    .setTitle(`Ticket Bot Help`)
+    .setColor(embedColor)
+    .setDescription(`Hello! I'm Ticket Manager, the Discord bot for super cool support ticket stuff! Here are my commands:`)
+    .addField(`Tickets`, `[${prefix}new]() > Opens up a new ticket and tags the Support Team\n[${prefix}close]() > Closes a ticket that has been resolved or been opened by accident\n[${prefix}add]() > Adds a member to a ticket\n[${prefix}remove]() > Removes a member from a ticket`)
+    message.channel.send({ embed: embed });
+  }
+
+
+  if (message.content.toLowerCase().startsWith(prefix + `new`)) {
+    const reason = message.content.split(" ").slice(1).join(" ");
+    if (!message.guild.roles.exists("name", "Support")) {
+    const embed0 = new Discord.RichEmbed()
+    .setColor(embedColor)
+    .addField(`Ticket Bot`, `This server doesn't have a \`Support\` role made, so the ticket won't be opened.\nIf you are an administrator, make one with that name exactly and give it to users that should be able to see tickets.`)
+    message.channel.send({ embed: embed0 });
+    return
+    }
+    if (message.guild.channels.exists("name", "ticket-" + message.author.username)) {
+    const embed1 = new Discord.RichEmbed()
+    .setColor(embedColor)
+    .addField(`Ticket Bot`, `You already have a ticket open.`)
+    message.channel.send({ embed: embed1 });
+    return
+    }
+    message.guild.createChannel(`ticket-${message.author.username}`, "text").then(c => {
+        let role = message.guild.roles.find("name", "Support");
+        let role2 = message.guild.roles.find("name", "@everyone");
+        c.overwritePermissions(role, {
+            SEND_MESSAGES: true,
+            READ_MESSAGES: true
+        });
+        c.overwritePermissions(role2, {
+            SEND_MESSAGES: false,
+            READ_MESSAGES: false
+        });
+        c.overwritePermissions(message.author, {
+            SEND_MESSAGES: true,
+            READ_MESSAGES: true
+        });
+        const embed2 = new Discord.RichEmbed()
+        .setColor(embedColor)
+        .addField(`Ticket Bot`, `Your ticket has been created in ` + c.toString())
+        .setTimestamp();
+        message.channel.send({ embed: embed2 });
+
+        const embed3 = new Discord.RichEmbed()
+        .setColor(embedColor)
+        .addField(`Hey ${message.author.username}!`, `Our **Support Team** will be with you shortly. Please explain your reason for opening the ticket in as much detail as possible.`)
+        .setTimestamp();
+        c.send({ embed: embed3 });
+    }).catch(console.error);
+  }
+
+  if (message.content.toLowerCase().startsWith(prefix + `add`)) {
+    if (!message.channel.name.startsWith(`ticket-`)) {
+    const embed4 = new Discord.RichEmbed()
+    .setColor(embedColor)
+    .addField(`Ticket Bot`, `You can't use the this outside of a ticket channel.`)
+    message.channel.send({ embed: embed4 });
+    return
+    }
+    addedmember = message.mentions.members.first();
+    message.channel.overwritePermissions(addedmember, { SEND_MESSAGES : true, VIEW_CHANNEL : true});
+    const embed5 = new Discord.RichEmbed()
+    .setColor(embedColor)
+    .addField(`Ticket Bot`, '**' + addedmember + `** has been added to the ticket. Remove with [${prefix}remove]().`)
+    message.channel.send({ embed: embed5 });
+
+  }
+
+  if (message.content.toLowerCase().startsWith(prefix + `remove`)) {
+    if (!message.channel.name.startsWith(`ticket-`)) {
+    const embed6 = new Discord.RichEmbed()
+    .setColor(embedColor)
+    .addField(`Ticket Bot`, `You can't use the this outside of a ticket channel.`)
+    message.channel.send({ embed: embed6 });
+    return
+    }
+    removedmember = message.mentions.members.first();
+    message.channel.overwritePermissions(removedmember, { SEND_MESSAGES : false, VIEW_CHANNEL : false});
+    const embed7 = new Discord.RichEmbed()
+    .setColor(embedColor)
+    .addField(`Ticket Bot`, '**' + removedmember + '** has been removed from the ticket.')
+    message.channel.send({ embed: embed7 });
+  }
+
+  if (message.content.toLowerCase().startsWith(prefix + `close`)) {
+    if (!message.channel.name.startsWith(`ticket-`)) {
+    const embed8 = new Discord.RichEmbed()
+    .setColor(embedColor)
+    .addField(`Ticket Bot`, `You can't use the this outside of a ticket channel.`)
+    message.channel.send({ embed: embed8 });
+    return
+    }   
+
+    const embed9 = new Discord.RichEmbed()
+    .setColor(embedColor)
+    .addField(`Ticket Bot`, 'Are you sure? Once confirmed, you cannot reverse this action!\nTo confirm, type \`-confirm\`. This will time out in 10 seconds and be cancelled.')
+    message.channel.send({ embed: embed9 })
+    .then((m) => {
+      message.channel.awaitMessages(response => response.content === '-confirm', {
+        max: 1,
+        time: 10000,
+        errors: ['time'],
+      })
+      .then((collected) => {
+          message.channel.delete();
+        })
+        .catch(() => {
+          m.edit('Ticket close timed out, the ticket was not closed.').then(m2 => {
+              m2.delete();
+          }, 3000);
+        });
+    });
+  }
+
+});
+
+function response(c) {
+  while (true) {
+    client.on("message", (message) => {
+      if(message.channel == c) {
+        return message.content;
+      }
+    });
+  }
+};
+
+client.login(token)
